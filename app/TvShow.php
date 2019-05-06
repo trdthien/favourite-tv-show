ebdf3879d479832e6ebd06ae114690f44b7b8bca
Trinh Duc Thien <trdthien@Trinhs-MacBook-Pro.local>
2019-05-05 23:16:04 -0400

    migration

---
+++ app/TvShow.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class TvShow
+ * @package App
+ * @property $id
+ * @property $season
+ * @property $episode
+ * @property $quote
+ */
+class TvShow extends Model
+{
+
+}
